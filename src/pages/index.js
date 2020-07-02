acaad5d94033d4d19adc433f3ccc2bc3fabc41bf
Mark <markw13a@gmail.com>
2020-07-02 22:11:46 +0100

    Adjusted intro and added an About section

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const Introduction = () => (
       I enjoy figuring out how to make life easier for businesses and their customers.
     </p>
     <p>
-      Whether you know what you want, or are just beginning to explore how technology could improve the way you do business, I can help. 
+      Whether you know what you want, or are just beginning to explore how an app or website could improve the way you do business, I can help. 
     </p>
   </>
 );
@@ -30,6 +30,18 @@ const RecentProjects = () => (
   </>
 );
 
+const AboutMe = () => (
+  <div className="about-me">
+    <h1> About me </h1>
+    <p>
+      I have worked as a salaried full-stack web developer for the last two and a half years. My off-hours are often spent working on my own projects, through which I have learned about mobile development, product development and design.
+    </p>
+    <p>
+      I am interested in the entire process of creating an app or website, and am always trying to broaden my skills. You can find out a bit more about me on my <a href="https://www.linkedin.com/in/mark-wood-922a8912a/"> Linkedin page</a>.
+    </p>
+  </div>
+);
+
 const Services = () => (
   <div className="services">
     <h1> Services </h1>
@@ -41,6 +53,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Introduction />
     <RecentProjects />
+    <AboutMe />
     <Services />
     <LinkButton text="Get in touch" to="/contact" />
   </Layout>
